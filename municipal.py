c1b5d895a7979da5b415e3e1b21be8c070e2a5e5
Miguel Expósito <exposito_m@cantabria.es>
2020-04-18 22:19:00 +0200

    Municipal data loaded into Firebase

--- municipal.py
+++ municipal.py
@@ -1,9 +1,11 @@
-import requests
-import pandas as pd
-
+"""Parse and load Cantabria's municipal covid19 data into Firebase."""
 from cfg import cfg
 
+import pandas as pd
+
 from pyjstat import pyjstat
+
+import requests
 
 import utils
 
@@ -15,19 +17,35 @@ rows = []
 for element in json_data['features']:
     rows.append(element['attributes'])
 data = pd.DataFrame(rows, columns=['OBJECTID_1', 'Codigo',
-                                    'Texto', 'Cod_Prov',
-                                    'Cod_CCAA', 'NumeroCasosActivos',
-                                    'NumeroCurados', 'NumeroFallecidos',
-                                    'NumeroCasos', 'Shape__Area',
-                                    'Shape__Length'])
+                                   'Texto', 'Cod_Prov',
+                                   'Cod_CCAA', 'NumeroCasosActivos',
+                                   'NumeroCurados', 'NumeroFallecidos',
+                                   'NumeroCasos', 'Shape__Area',
+                                   'Shape__Length'])
+population = pd.read_csv(cfg.input.path + cfg.input.population)
 data.sort_values(by=['Codigo'], inplace=True)
 data.reset_index(inplace=True)
 if data.at[52, 'Texto'] == '':
     data.at[52, 'Texto'] = 'Polaciones'
 
+population['Codigo'] = population['Codigo'].apply(str)
+data = pd.merge(data, population, on='Codigo')
+data['Tasa bruta de casos'] = (
+    data['NumeroCasos'] / data['poblacion']) * 100000
+data['Tasa bruta de activos'] = (
+    data['NumeroCasosActivos'] / data['poblacion']) * 100000
+data['Tasa bruta de altas'] = (
+    data['NumeroCurados'] / data['poblacion']) * 100000
+data['Tasa bruta de fallecidos'] = (
+    data['NumeroFallecidos'] / data['poblacion']) * 100000
+print(data)
+
 measures = {}
 datasets = {}
 jsonstat = {}
+
+utils.initialize_firebase_db(cfg.firebase.creds_path, cfg.firebase.db_url)
+
 for measure in cfg.municipalities.measures:
     measures[measure] = data[['Codigo',
                               'Texto',
@@ -35,9 +53,9 @@ for measure in cfg.municipalities.measures:
     measures[measure]['municipios'] = measures[measure]['Codigo'] + \
         ' - ' + measures[measure]['Texto']
     measures[measure].drop(columns=['Codigo', 'Texto'], inplace=True)
-    measures[measure].rename(columns={cfg.municipalities.measures[measure].original:
-                                      cfg.municipalities.measures[measure].final},
-                             inplace=True)
+    measures[measure].rename(
+        columns={cfg.municipalities.measures[measure].original:
+                 cfg.municipalities.measures[measure].final}, inplace=True)
     measures[measure] = measures[measure].melt(
         id_vars='municipios',
         value_vars=[cfg.municipalities.measures[measure].final],
@@ -47,24 +65,7 @@ for measure in cfg.municipalities.measures:
                                                      ' del Gobierno de '
                                                      'Cantabria'))
     datasets[measure]["role"] = {"metric": ["Variables"]}
-    jsonstat[measure] = datasets[measure].write()
-
-files = {
-    cfg.output.municipalities.cases: {
-        "content": jsonstat['cases']
-    },
-    cfg.output.municipalities.actives: {
-        "content": jsonstat['actives']
-    },
-    cfg.output.municipalities.deceased: {
-        "content": jsonstat['deceased']
-    },
-    cfg.output.municipalities.discharged: {
-        "content": jsonstat['discharged']
-    }
-}
-
-utils.publish_gist(files,
-                   cfg.labels.municipalities_absolute_gist,
-                   cfg.github.municipalities_absolute_gist_id)
-print(files)
+    utils.publish_firebase('saludcantabria',
+                           cfg.output.municipalities[measure],
+                           datasets[measure])
+    print(datasets[measure].write())
